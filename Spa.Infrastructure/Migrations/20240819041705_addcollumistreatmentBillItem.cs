94b91691acdb612388aa7b19ba7cf46a9ae045a9
Khoi Hoang <lahoangkhoi.ck@gmail.com>
2024-08-19 11:17:38 +0700

    add collum istreatment in billItem

---
+++ Spa.Infrastructure/Migrations/20240819041705_addcollumistreatmentBillItem.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Spa.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addcollumistreatmentBillItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PartnerName",
+                table: "IncomeExpenses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "BranchID",
+                table: "IncomeExpenses",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Amount",
+                table: "IncomeExpenses",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsTreatment",
+                table: "BillItem",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsTreatment",
+                table: "BillItem");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PartnerName",
+                table: "IncomeExpenses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "BranchID",
+                table: "IncomeExpenses",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Amount",
+                table: "IncomeExpenses",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+        }
+    }
+}
